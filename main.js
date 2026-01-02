3549ab8bc2bf12c307687286a13047bb4fabc135
OdinM13 <markodinius@gmail.com>
2026-01-02 21:12:39 +0100

    Setup templates for html, js and css

---
+++ main.js
@@ -0,0 +1,7 @@
+// add function
+// substract function
+// multiply function
+// divide function
+// 3 variables for each: Number, Operator, another Number
+// Operate Function
+// Function for display KeyStrokes
