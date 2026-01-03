fd9d286b16c90703b5d2f7394a49e162308caba3
OdinM13 <markodinius@gmail.com>
2026-01-03 20:05:29 +0100

    Build EventListener and working calculating function

--- main.js
+++ main.js
@@ -1,33 +1,60 @@
 // 3 variables for each: Number, Operator, another Number
 let firstNumber = null;
 let secondNumber = null;
-// let operator = "";
+let helpingNumber = "";
+let firstOperator = "";
+let secondOperator = "";
 
+const display = document.querySelector("#display > p");
 let numbers = document.querySelectorAll("#numbers > button");
 console.log(numbers);
+let operators = document.querySelectorAll("#operators > button");
+console.log(operators);
+
 
 numbers.forEach(number => {
     number.addEventListener("click", () => {
         const selectedId = number.id 
         numberClick(selectedId);
+        console.log(helpingNumber);
+    });
+});
+
+operators.forEach(opera => {
+    opera.addEventListener("click", () => {
+        const selectedId = opera.id 
+        operatorClick(selectedId);
         console.log(firstNumber);
         console.log(secondNumber);
+        console.log(firstOperator);
+        console.log(secondOperator);
     });
 });
 
 function numberClick(id) {
-    if (firstNumber === null) {
-        firstNumber = id; 
-        return firstNumber;
-    } else if (secondNumber === null) {
-        secondNumber = id;
-        return secondNumber;
-    }
-    // } else {
-    //     operate(firstNumber, secondNumber, operator);
-    // }
+    return helpingNumber += id;
 }
 
+function operatorClick (id) {
+    if (firstOperator === "") {
+        firstOperator = id;
+    } else {
+        secondOperator = id;
+    }
+    if (firstNumber === null) {
+        firstNumber = Number(helpingNumber); 
+        helpingNumber = "";
+        // return firstNumber;
+    } else if (secondNumber === null) {
+        secondNumber = Number(helpingNumber);
+        helpingNumber = "";
+        operate(firstNumber, secondNumber, firstOperator);
+        // return secondNumber;
+    } 
+    // else {
+    //     operate(firstNumber, secondNumber, firstOperator);
+    // }
+}
 
 // add function
 function add (a, b) {
@@ -49,20 +76,26 @@ function divide (a, b) {
 }
 
 // operate function
-// function operate(numb1, numb2, operat) {
-//     if (operat === ) {
-//         add(numb1, numb2);
-//     }
-//     if (operat === ) {
-//         substract(numb1, numb2);
-//     }
-//     if (operat === ) {
-//         multiply(numb1, numb2);
-//     }
-//     if (operat === ) {
-//         divide(numb1, numb2);
-//     }
-//
-// }
+function operate(numb1, numb2, operat) {
+    let result = 0;
+    if (operat === "add") {
+        result = add(numb1, numb2);
+    }
+    if (operat === "substract") {
+        result = substract(numb1, numb2);
+    }
+    if (operat === "multiply") {
+        result = multiply(numb1, numb2);
+    }
+    if (operat === "divide") {
+        result = divide(numb1, numb2);
+    }
+    firstNumber = result;
+    firstOperator = secondOperator;
+    secondOperator = "";
+    secondNumber = null;
+    console.log(result);
+    display.innerText = result;
+};
 
 // Function for display KeyStrokes
